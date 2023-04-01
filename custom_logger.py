c0f35dc2645f6d87a3f5568983f9155c4fecba25
Reece Nicholls <33102368+ReeceNich@users.noreply.github.com>
2023-04-01 20:23:34 +0100

    change where log files are written

--- custom_logger.py
+++ custom_logger.py
@@ -5,7 +5,31 @@ class CustomLogger(Logger):
         atoms = self.atoms(resp, req, environ, request_time)
         if resp.status.startswith("200"):
             self.access_log.info(self.cfg.access_log_format % atoms)
-        elif resp.status.startswith("404") or resp.status.startswith("5"):
-            self.error_log.info(self.cfg.access_log_format % atoms)
         else:
-            self.log.info(self.cfg.access_log_format % atoms)
+            self.error_log.info(self.cfg.access_log_format % atoms)
+
+logconfig_dict = {
+    "version": 1,
+    "handlers": {
+        "access_200_file": {
+            "class": "logging.FileHandler",
+            "filename": "access_200.log",
+            "formatter": "generic",
+        },
+        "access_other_file": {
+            "class": "logging.FileHandler",
+            "filename": "access_other.log",
+            "formatter": "generic",
+        },
+    },
+    "loggers": {
+        "gunicorn.access": {
+            "handlers": ["access_200_file"],
+            "level": "INFO",
+        },
+        "gunicorn.error": {
+            "handlers": ["access_other_file"],
+            "level": "INFO",
+        },
+    },
+}
